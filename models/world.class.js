e9e37e050b13adf907dac987d86a566c8691a3b8
Baris Aslan <baris7801@hotmail.de>
2022-07-07 18:06:54 +0200

    character, enemies, background added

---
+++ models/world.class.js
@@ -0,0 +1,47 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 100)
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.addObjectsToMap(this.character);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.backgroundObjects);
+
+        // draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(obj => {
+            this.addToMap(obj);
+        });
+    }
+
+    addToMap(movableObject) {
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+    }
+}
