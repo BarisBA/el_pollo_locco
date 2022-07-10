6b31ced813c4f766b1049134b317b6c196f342b2
Baris Aslan <baris7801@hotmail.de>
2022-07-10 18:24:03 +0200

    statusBar added

--- models/world.class.js
+++ models/world.class.js
@@ -1,10 +1,11 @@
-class World {
+class World{
     character = new Character();
     level = level1;
     canvas;
     ctx;
     keyboard; 
     camera_x = 0;
+    statusBar = new StatusBar();
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -24,7 +25,7 @@ class World {
             this.level.enemies.forEach(enemy => {
                 if (this.character.isColliding(enemy)) {
                     this.character.hit();
-                    console.log(this.character.energy)
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             });
         }, 200);
@@ -34,8 +35,13 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.backgroundObjects);
+
+        this.ctx.translate(-this.camera_x, 0);
+        // space for fixed objects
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
